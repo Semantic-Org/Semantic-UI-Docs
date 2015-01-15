d40b6eba967df5e013c5b4394a4464a69e27d1a0
jlukic <jacklukic@gmail.com>
2015-01-15 18:11:49 -0500

    Fix mistake in code indent

--- server/files/javascript/docs.js
+++ server/files/javascript/docs.js
@@ -602,9 +602,14 @@ semantic.ready = function() {
           ? lines[1]
           : lines[0],
         spacesPerIndent = 2,
-        leadingSpaces   = firstLine.length - firstLine.replace(/^\s*/g, '').length,
+        leadingSpaces   = (firstLine !== undefined)
+          ? firstLine.length - firstLine.replace(/^\s*/g, '').length
+          : false,
         indent
       ;
+      if(!leadingSpaces) {
+        return 4;
+      }
       if(leadingSpaces !== 0) {
         indent = leadingSpaces;
       }
